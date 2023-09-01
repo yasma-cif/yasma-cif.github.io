0637442cfc3506ba62c6913fb5fb086bcff75a6f
Leamsy <ismamoyano97@gmail.com>
2023-09-01 18:10:05 +0200

    init template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://leamsy.github.io',
-})
+export default defineConfig({});
