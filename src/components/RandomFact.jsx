3e08093aae4d2170cf4a56b7e9c1e6e046c31ae0
Isma <ismamoyano97@gmail.com>
2024-11-07 22:24:50 +0100

    Rework full!

---
+++ src/components/RandomFact.jsx
@@ -0,0 +1,38 @@
+﻿import { useState } from 'preact/hooks';
+
+let lastNum = -1;
+
+export default function RandomFact({messages}) {
+
+
+    const randomMessage = () => 
+    {
+        let result = Math.floor(Math.random() * messages.length);
+
+        while(result == lastNum)
+        {
+            result = Math.floor(Math.random() * messages.length);
+        }
+
+        lastNum = result;
+        
+        
+        return messages[(result)];
+    }
+
+    const [greeting, setGreeting] = useState(randomMessage());
+    
+
+    return (
+        <div style="text-align: center;">
+            <div class="human-facts">
+                <p>
+                    <b>{greeting}</b>
+                </p>
+            </div>
+            <button class="button" style="margin-top: 30px" onClick={() => setGreeting(randomMessage())}>
+                Re-fact
+            </button>
+        </div>
+    );
+}
